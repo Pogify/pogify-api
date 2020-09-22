ff22ae3da9701bf8c0907b7c979d3f994d646844
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-09-22 09:02:27 -0400

    missing env vars will error auth endpoint

--- v1/twitchAuth.go
+++ v1/twitchAuth.go
@@ -8,17 +8,25 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var twitchAuthDisable = false
+
 func init() {
 	if os.Getenv("TWITCH_CLIENT_ID") == "" {
-		log.Print("missing TWITCH_CLIENT_ID in .env")
+		log.Print("missing TWITCH_CLIENT_ID in .env. Calls to authenticate with twitch will error")
+		twitchAuthDisable = true
 	}
 	if os.Getenv("TWITCH_CLIENT_SECRET") == "" {
-		log.Print("missing TWITCH_CLIENT_SECRET in .env")
-
+		log.Print("missing TWITCH_CLIENT_SECRET in .env. Calls to authenticate with twitch will error")
+		twitchAuthDisable = true
 	}
 }
 
 func (s *server) twitchAuth(c *gin.Context) {
+	if twitchAuthDisable {
+		c.String(503, "Server not configured for twitch authentication")
+		return
+	}
+
 	req, err := http.NewRequest("POST", "https://id.twitch.tv/oauth2/token", nil)
 	if err != nil {
 		c.AbortWithError(500, err)
